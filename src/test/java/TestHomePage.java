6dd7b4db803b2c07243ae2c72ceefd21c20fa72c
Anastasiia <asenka88@gmail.com>
2022-04-22 00:57:18 +0300

    Test-is that right page?

--- src/test/java/TestHomePage.java
+++ src/test/java/TestHomePage.java
@@ -1,6 +1,16 @@
 import org.testng.annotations.Test;
 
+import static org.testng.AssertJUnit.assertEquals;
+
 public class TestHomePage extends SpeakUkrainianRunner{
+    @Test
+    public void testActualPage() {
+        System.out.println("Check pageURL");
+        assertEquals("https://speak-ukrainian.org.ua/dev/", driver.getCurrentUrl());
+
+        System.out.println("Check page title");
+        assertEquals("Навчай українською", driver.getTitle());
+    }
 
     @Test
     public void Check(){
