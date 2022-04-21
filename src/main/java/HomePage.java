33c00148c259a9e6e4e892f2bf999321fe87f649
Volodja Zem <v.zemela@gmail.com>
2022-04-21 18:11:18 +0300

    create SpeakUkrainianRunner

---
+++ src/main/java/HomePage.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends TopPage {
+    public HomePage(WebDriver driver) {
+        super(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+}
