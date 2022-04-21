8ffccbd730532f427ed6bae7dca2bf0d727d95a3
Anastasiia <asenka88@gmail.com>
2022-04-22 00:45:29 +0300

    Added Data

---
+++ src/main/java/Data.java
@@ -0,0 +1,38 @@
+public class Data {
+        private String email;
+        private String password;
+
+
+
+        public Data(){
+            email = " admin@gmail.com";
+            password = "admin";
+        }
+
+        // setters
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+
+        // getters
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+
+        @Override
+        public String toString() {
+            return "User [email=" + email + ", password=" + password + "]";
+        }
+}
