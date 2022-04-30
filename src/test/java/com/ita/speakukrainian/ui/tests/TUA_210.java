bf4300be63b9d3c2fc5f3a3a6981350a713946ac
DGalak <dmytrohalak07@gmail.com>
2022-05-01 00:44:23 +0300

    Implement TUA_210

---
+++ src/test/java/com/ita/speakukrainian/ui/tests/TUA_210.java
@@ -0,0 +1,30 @@
+package com.ita.speakukrainian.ui.tests;
+
+import com.ita.speakukrainian.ui.SpeakUkrainianRunner;
+import com.ita.speakukrainian.ui.pages.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TUA_210 extends SpeakUkrainianRunner {
+
+    @Test
+    public void verifyInputAcceptsPositiveIntegersFrom2To18() {
+        String actualAgeValue = new HomePage(driver)
+                .header().clickExtendedSearchButton()
+                .getExtendedSearchComponent().fillInAgeInput(2)
+                .getExtendedSearchComponent().readAgeInput();
+
+       Assert.assertEquals(Integer.valueOf(actualAgeValue),2);
+    }
+
+    @Test
+    public void verifyInputAcceptsPositiveIntegersFrom2To182() {
+        String actualAgeValue = new HomePage(driver)
+                .header().clickExtendedSearchButton()
+                .getExtendedSearchComponent().fillInAgeInput(20)
+                .getExtendedSearchComponent().readAgeInput();
+
+        Assert.assertEquals(Integer.valueOf(actualAgeValue),18);
+    }
+}
+
