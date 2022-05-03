1161a8afba89b5074505c1fe48fbfe593ee97d15
Anastasiia <asenka88@gmail.com>
2022-05-03 17:33:20 +0300

    Midified test

--- src/test/java/com/ita/speakukrainian/ui/tests/TestIncorrectNameOfCenter252.java
+++ src/test/java/com/ita/speakukrainian/ui/tests/TestIncorrectNameOfCenter252.java
@@ -1,11 +1,8 @@
 package com.ita.speakukrainian.ui.tests;
 
 import com.ita.speakukrainian.ui.SpeakUkrainianRunner;
-import com.ita.speakukrainian.ui.components.HeaderMenuComponent;
 import com.ita.speakukrainian.ui.pages.HomePage;
-import com.ita.speakukrainian.ui.pages.MyProfilePage;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestIncorrectNameOfCenter252 extends SpeakUkrainianRunner {
@@ -27,7 +24,6 @@ public class TestIncorrectNameOfCenter252 extends SpeakUkrainianRunner {
                 .clickAddCenterButton()
                 .clickNextStepButton()
                 .getError();
-
         Assert.assertEquals(incorrectCenterName, "Некоректна назва центру");
     }
 }
