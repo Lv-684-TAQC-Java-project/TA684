46bb88c3a9e1ba96d8bdbe824c07d04c509866b2
Anastasiia <asenka88@gmail.com>
2022-05-07 21:15:23 +0300

    Revrite test160

--- src/test/java/com/ita/speakukrainian/ui/tests/TestDataLocationIsEmpty160.java
+++ src/test/java/com/ita/speakukrainian/ui/tests/TestDataLocationIsEmpty160.java
@@ -1,29 +1,38 @@
 package com.ita.speakukrainian.ui.tests;
 
 import com.ita.speakukrainian.ui.SpeakUkrainianRunner;
+import com.ita.speakukrainian.ui.components.HeaderMenuComponent;
 import com.ita.speakukrainian.ui.pages.HomePage;
 import com.ita.speakukrainian.ui.popup.LocationPopUp;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestDataLocationIsEmpty160 extends SpeakUkrainianRunner {
-    @Test
-    public void CheckButtonIsUnavailable() {
-       String buttonIsDisabled = new HomePage(driver)
+    @BeforeMethod
+    public void toTest160 (){
+        setUp();
+         new HomePage(driver)
                 .header()
                 .clickUserProFileButton()
                 .clickSingInButton()
                 .sendKeysEmail(valueProvider.getAdminEmail())
                 .sendKeysPassword(valueProvider.getAdminPassword())
-                .clickLoginButton()
+                .clickLoginButton();
+
+    }
+
+    @Test
+    public void CheckButtonIsUnavailable() {
+        LocationPopUp locationPopUp = new HomePage(driver)
                 .header()
                 .clickUserProFileButton()
                 .clickAddCenterButton()
-                .clickAddLocation()
-                .clickAddInLocationButton();
+                .clickAddLocation();
 
-                Assert.assertEquals(buttonIsDisabled, "Button is Disabled");
+
+                Assert.assertFalse(locationPopUp.isDesabledAddLocationsButton(), "Button is not disabled");
                 //Assert.assertFalse(addInLocationButton.isEnable());
     }
 }
