3f62283e5413ef476b4b9277ab49c11b7299090a
Liubomyr Halamaha <galamagal86@gmail.com>
2022-05-17 12:47:02 +0300

    added testNGLisener

--- src/test/java/com/ita/speakukrainian/ui/SpeakUkrainianRunner.java
+++ src/test/java/com/ita/speakukrainian/ui/SpeakUkrainianRunner.java
@@ -1,16 +1,18 @@
 package com.ita.speakukrainian.ui;
 
+import com.ita.speakukrainian.utils.TestNGListener;
 import com.ita.speakukrainian.utils.ValueProvider;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
+import org.testng.ITestContext;
+import org.testng.annotations.*;
 
 import java.io.IOException;
 import java.time.Duration;
 
+
+@Listeners(TestNGListener.class)
 public class SpeakUkrainianRunner {
 
     protected static WebDriver driver;
@@ -27,9 +29,10 @@ public class SpeakUkrainianRunner {
 
 
     @BeforeMethod
-    public void setUp() {
+    public void setUp(ITestContext context) {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        context.setAttribute("myDriver", driver);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         driver.get(valueProvider.getBaseURL());
@@ -37,6 +40,12 @@ public class SpeakUkrainianRunner {
 
     @AfterMethod(alwaysRun = true)
     public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+    @AfterSuite
+    public void afterSuite() throws IOException {
         if (driver != null) {
             driver.quit();
         }
