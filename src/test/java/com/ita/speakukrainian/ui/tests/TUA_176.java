a8b388240aaf6afd822a9c3b2fbc28fba34efa80
Anton Shcherbei <starteran2gmail.com>
2022-05-20 14:53:09 +0300

    add new runner

--- src/test/java/com/ita/speakukrainian/ui/tests/TUA_176.java
+++ src/test/java/com/ita/speakukrainian/ui/tests/TUA_176.java
@@ -1,15 +1,16 @@
 package com.ita.speakukrainian.ui.tests;
 
+import com.ita.speakukrainian.ui.SpeakUkrainianLoginMainPageRunner;
 import com.ita.speakukrainian.ui.SpeakUkrainianRunner;
+import com.ita.speakukrainian.ui.pages.AddClubPages.Explanation;
 import com.ita.speakukrainian.ui.pages.HomePage;
+import io.qameta.allure.Description;
 import io.qameta.allure.Issue;
-import jdk.jfr.Description;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
-public class TUA_176 extends SpeakUkrainianRunner {
-
+public class TUA_176 extends SpeakUkrainianLoginMainPageRunner {
     @DataProvider(name = "data")
     public Object[][] dataProvider() {
         Object[][] data = new Object[][]{
@@ -25,26 +26,7 @@ public class TUA_176 extends SpeakUkrainianRunner {
     @Description("[allure] Not valid enter phone number ")
     @Issue("TUA-224")
     public void testNotValidEnterPhoneNumber(String testCaseValue, String expected) {
-        String errorMassage = new HomePage(driver)
-                .header()
-                .clickUserProFileButton()
-                .clickSingInButton()
-                .sendKeysEmail(valueProvider.getAdminEmail())
-                .sendKeysPassword(valueProvider.getAdminPassword())
-                .clickLoginButton()
-                .header()
-                .clickUserProFileButton()
-                .clickMyProfileButton()
-                .clickAddButton()
-                .clickAddClubButton()
-                .fillInClubNameInput("Football")
-                .clickOptionCheckboxes(2)
-                .clickOptionCheckboxes(1)
-                .fillInAgeFromInput("2")
-                .fillInAgeToInput("18")
-                .clickNextStepButton()
-                .fillInContactPhoneInput(valueProvider.getContactPhoneNumber())
-                .clickNextStepButton()
+        String errorMassage = new Explanation(driver)
                 .fillInBasicDescriptionInput(testCaseValue)
                 .getWrongDescriptionAlert();
         SoftAssert softAssert = new SoftAssert();
