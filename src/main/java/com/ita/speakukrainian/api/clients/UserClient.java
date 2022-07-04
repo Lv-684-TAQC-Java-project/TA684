4ff726ff718dce217214ad825eb3cd6642ed68d0
Volodja Zem <v.zemela@gmail.com>
2022-07-04 13:22:01 +0300

    create test 411

--- src/main/java/com/ita/speakukrainian/api/clients/UserClient.java
+++ src/main/java/com/ita/speakukrainian/api/clients/UserClient.java
@@ -1,6 +1,7 @@
 package com.ita.speakukrainian.api.clients;
 
 import com.ita.speakukrainian.api.models.user.CreatedUserRequest;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 
 public class UserClient extends BaseClient {
@@ -9,7 +10,7 @@ public class UserClient extends BaseClient {
 
     public UserClient(String authorizationToken) {
         super();
-        this.authorizationToken=authorizationToken;
+        this.authorizationToken = authorizationToken;
     }
 
     public Response put(CreatedUserRequest body) {
@@ -18,5 +19,14 @@ public class UserClient extends BaseClient {
                 .when()
                 .body(body)
                 .put(String.format("%s%s", this.baseUrl, this.path));
+    }
+
+    @Step("Update user by {id}")
+    public Response put(int id, CreatedUserRequest body) {
+        return prepareRequest()
+                .header("Authorization", String.format("Bearer %s", authorizationToken))
+                .body(body)
+                .when()
+                .put(String.format("%s/api/user/%s", baseUrl, id));
     }
 }
