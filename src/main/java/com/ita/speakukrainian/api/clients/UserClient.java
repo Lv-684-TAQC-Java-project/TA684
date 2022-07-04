b88dc3aab46eff8ac7ea857a9b889bd898ff5e0b
DGalak <dmytrohalak07@gmail.com>
2022-07-04 14:26:03 +0300

    impruve tua 408

--- src/main/java/com/ita/speakukrainian/api/clients/UserClient.java
+++ src/main/java/com/ita/speakukrainian/api/clients/UserClient.java
@@ -5,18 +5,18 @@ import io.restassured.response.Response;
 
 public class UserClient extends BaseClient {
     private String authorizationToken;
-    private final String path = "/api/user/203";
+    private final String path = "/api/user";
 
     public UserClient(String authorizationToken) {
         super();
         this.authorizationToken=authorizationToken;
     }
 
-    public Response put(CreatedUserRequest body) {
+    public Response put(CreatedUserRequest body,int id) {
         return prepareRequest()
                 .header("Authorization", String.format("Bearer %s", this.authorizationToken))
                 .when()
                 .body(body)
-                .put(String.format("%s%s", this.baseUrl, this.path));
+                .put(String.format("%s%s/%s", this.baseUrl, this.path,id));
     }
 }
